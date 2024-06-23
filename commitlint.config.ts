702ab329a732672133ca589f4c6300a66dcc9021
hyunwlee <hwlee3928@gmail.com>
2024-06-23 13:13:07 +0900

    chore(lint): add client hook - commit-msg, prepare-commit-msg

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,6 +3,11 @@ import type { UserConfig } from '@commitlint/types';
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-max-length': [2, 'always', 72],
+    'body-leading-blank': [2, 'always'],
+    'body-max-length': [1, 'always', 400],
+    'footer-leading-blank': [2, 'always'],
+    'type-case': [2, 'always', 'lower-case'],
     'type-enum': [
       2,
       'always',
@@ -21,7 +26,6 @@ const Configuration: UserConfig = {
         'perf',
       ],
     ],
-    'type-case': [2, 'always', 'lower-case'],
   },
 };
 
