2e5a614186f536d997834a89c740b4b25a8cf739
hyunwlee <hwlee3928@gmail.com>
2024-06-26 15:31:02 +0900

    fix: remove necessary import

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
 export type { PropsWithClassName } from './props';
 export type { NonEmptyArray } from './types';
 export type { ServicesType, FacilitiesType, SurveyType } from './survey';
-export type { SurveyInfo, LocalStorageType } from './local-storage';
+export type { SurveyInfo } from './local-storage';
